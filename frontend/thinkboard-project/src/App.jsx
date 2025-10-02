2d41611b3ded8301a1b14543fdef63b2d5e67148
Matheus dos Santos <matheus.dso1996@gmail.com>
2025-10-02 17:25:37 -0300

    Commit: ThinkBoard FrontEnd configuration (v1.44.49)

---
+++ frontend/thinkboard-project/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Route, Routes } from 'react-router'
+
+const App = () => {
+  return <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      <Routes/>
+    </div>;
+};
+
+export default App;
